c2fb5a9a8d028df3d010ca7a1a8619fb8726db94
Haven Fricke <robslaven@live.com>
2023-03-20 13:40:37 -0600

    push

--- src/env.js
+++ src/env.js
@@ -1,1 +1,6 @@
-//env
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = ''
+export const clientId = ''
+export const audience = ''
